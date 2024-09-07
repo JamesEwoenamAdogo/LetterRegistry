dfa96dac3ff44b867182c43cac3e2b5625253ea4
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2024-09-07 14:51:24 +0000

    Update index.js

--- api/api/index.js
+++ api/api/index.js
@@ -13,7 +13,9 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT
 
-app.use(cors())
+app.use(cors({
+    origin:"https://letter-registry-7o28.vercel.app/"
+}))
 app.use(express.json())
 app.listen(port ,()=>{
     console.log(`Server running at ${port}`)
